3b0dee2c68d84f243f58e8a1634bab61b9942679
iAlijon <93911401+iAlijon@users.noreply.github.com>
2026-04-29 16:56:18 +0500

    last

--- resources/views/admin/premiere/index.blade.php
+++ resources/views/admin/premiere/index.blade.php
@@ -9,7 +9,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('premiere.index')}}">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{route('film_digest.index')}}">Home</a></li>
                         <li class="breadcrumb-item active">Premiere</li>
                     </ol>
                 </div>
